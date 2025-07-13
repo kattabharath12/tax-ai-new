5147374b5e5a7bd9cd9699136fb0ea23fddbb30d
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-13 11:01:43 -0500

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
 from sqlalchemy.sql import func
-from ..core.database import Base
+from core.database import Base
 
 class User(Base):
     __tablename__ = "users"
