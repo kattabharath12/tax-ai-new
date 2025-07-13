e69cddafc6f6ac1ae2581c76ee8dec49a29195ea
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-13 11:14:09 -0500

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from models.user import User
 from models.tax_form import TaxForm
-from document import Document
+from models.document import Document
 from payment import Payment
 
 from core.database import Base
