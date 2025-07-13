64e63cd8da9c2089d3fde8d9d9a5d478e7b7bc1c
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-13 10:56:36 -0500

    Update payments.py

--- backend/app/api/api_v1/endpoints/payments.py
+++ backend/app/api/api_v1/endpoints/payments.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from ...core.database import get_db
-from ...models.payment import Payment
+from core.database import get_db
+from models.payment import Payment
 
 router = APIRouter()
 
