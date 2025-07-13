d8d0d72ad5612f7928d3254db7e2ec4c93411258
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-13 11:01:04 -0500

    Update document.py

--- backend/app/models/document.py
+++ backend/app/models/document.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-from ..core.database import Base
+from core.database import Base
 
 class Document(Base):
     __tablename__ = "documents"
